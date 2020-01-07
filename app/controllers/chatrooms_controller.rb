df75c306ff4a7bec2eea34624a4d05b4804433e5
nuri1977 <nurilacka@gmail.com>
2020-01-07 20:59:36 +0100

    semantuic ui gem, root

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,16 @@
+class ChatroomsController < ApplicationController
+
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'chatrooms#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
