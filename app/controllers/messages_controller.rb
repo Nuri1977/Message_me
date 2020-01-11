bcbd8d29a040602db44ab4b0de717dc03f7ff2ba
nuri1977 <nurilacka@gmail.com>
2020-01-11 20:14:02 +0100

    chatroom chanel created and messages created

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path     
+      ActionCable.server.broadcast "chatroom_channel", mode_message: message.body
     end 
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 
+  mount ActionCable.server, at: '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
